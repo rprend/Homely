c339317ec25509e47c10879a1d6772e8459c6f3c
Theodore Li <theodoreqili@gmail.com>
2019-09-28 14:44:13 -0500

    Convert explore to fragment

--- app/src/main/java/org/homely/MainActivity.java
+++ app/src/main/java/org/homely/MainActivity.java
@@ -7,23 +7,24 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.homely.explore_activity.ExploreFragment;
-import org.homely.explore_activity.HouseAdapter;
-import org.homely.house_activity.HouseActivity;
 
 public class MainActivity extends AppCompatActivity implements ExploreFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -36,6 +37,12 @@ public class MainActivity extends AppCompatActivity implements ExploreFragment.O
         });
 
 
+        FrameLayout frame = findViewById(R.id.fragmentPlaceholder);
+        if (savedInstanceState == null) {
+            Fragment newFragment = new ExploreFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.fragmentPlaceholder, newFragment).commit();
+        }
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.exploreToolbar);
         setSupportActionBar(myToolbar);
